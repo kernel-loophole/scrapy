89c454dc1b1ca42e6c4d462865afb6493d6bdbad
kernel_loophole <p190119@nu.edu.pk>
2022-09-21 14:33:16 -0400

    tages

--- bs4/object_kind.py
+++ bs4/object_kind.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 with open('index.html') as f:
     soup=BeautifulSoup(f)
-# tages=soup.p
-# print(type(tages))
+tages=soup.p
+tages.name="test"
+try:
+    print(tages['id'])
+except:
+    print("error")
+del tages
